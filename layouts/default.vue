cf3ee744d6fa4a246082a3e3cf09357d1d19b732
=elirehema <=rocksandystone@gmail>
2021-01-27 17:42:35 +0300

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app class="app">
+    <v-app-bar dense flat absolute hide-on-scroll app>
+      <v-toolbar-title>
+        <strong class="text--primary">Ospic Documentation</strong>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <nuxt class="ma-0 pa-0" />
+      </v-container>
+    </v-main>
+    <v-footer-component></v-footer-component>
+  </v-app>
+</template>
+
+<script>
+import FooterComponent from "@/components/global/Footer";
+export default {
+  components: {
+    VFooterComponent: FooterComponent,
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      message: null,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js",
+    };
+  },
+};
+</script>
+<style>
+.app {
+  font-family: "Nunito", sans-serif;
+}
+</style>
