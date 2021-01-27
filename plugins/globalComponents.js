16221ed7a9258145635afc6d6ebdd96a7382f934
=elirehema <=rocksandystone@gmail.com>
2021-01-27 21:16:04 +0300

    Initial commit

--- plugins/globalComponents.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import VImg from "~/components/VImg.vue";
-import VTags from "~/components/VTags.vue";
-import PostCard from "~/components/PostCard";
-
-Vue.component("VImg", VImg);
-Vue.component("VTags", VTags);
-Vue.component("PostCard", PostCard);
