c15b6902cdf071fbf57505b6ad25c7ce232ada5d
=elirehema <=rocksandystone@gmail.com>
2021-01-27 22:55:06 +0300

    Documentation home page

---
+++ content/home/my-home-page.md
@@ -0,0 +1,12 @@
+---
+
+title: Ospic HMS documentation
+description: Official Ospic HMS documentation
+img: home-page.png
+alt: home page screenshot
+
+---
+
+# Home page
+
+Welcome to  ospic documentation
--- layouts/default.vue
+++ layouts/default.vue
@@ -26,29 +26,13 @@
     </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
-      fixed
-      app
+      fixed flat
+      app dense
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+    
+    
+      <v-toolbar-title class="primary--text"> <strong>{{ title }}</strong></v-toolbar-title>
       <v-spacer />
       <v-btn
         icon
@@ -110,7 +94,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Ospic documentation'
     }
   }
 }
