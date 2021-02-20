535b27f82517b270fbc3b2df52794fb28df70e67
=elirehema <=rocksandystone@gmail.com>
2021-02-20 16:23:05 +0300

    All documents under the same file

--- pages/docs/_slug.vue
+++ pages/docs/_slug.vue
@@ -8,7 +8,7 @@
       </v-col>
 
       <v-col cols="12" sm="8">
-        <v-sheet min-height="70vh" class="pa-5">
+        <v-sheet min-height="120vh" class="pa-5">
           <div>
             <h1 class="h1 post-h1">{{ post.title }}</h1>
             <p v-if="post.description" class="excerpt">
@@ -26,7 +26,7 @@
       </v-col>
 
       <v-col cols="12" sm="2">
-        <v-sheet class="pa-5" rounded="lg" min-height="268">
+        <v-sheet rounded="lg" min-height="268" class="pa-5">
           <v-pages :pages="pages"></v-pages>
         </v-sheet>
       </v-col>
@@ -35,51 +35,13 @@
 </template>
 <script>
 export default {
-  async asyncData({ params, error, $content }) {
-    try {
-      const postPath = `/docs/index`;
-      const [post] = await $content("docs", params.slug).fetch();
+  async asyncData({ $content, params }) {
+    const post = await $content("docs", params.slug).fetch();
 
-      const pages = await $content("doc")
-        .only(["title", "description", "img", "slug", "author"])
-        .fetch();
-      console.log(articles);
-
-      return { post, pages };
-    } catch (err) {
-      error({
-        statusCode: 404,
-        message: "Page could not be found"
-      });
-    }
-  },
-
-  mounted() {
-    Prism.highlightAll();
-  },
-  head() {
-    return {
-      title: this.post.title,
-      meta: [
-        {
-          hid: "description",
-          name: "description",
-          content: this.post.description
-        }
-      ],
-      link: [
-        {
-          rel: "canonical",
-          href: "https://matthewblewitt.com/posts/" + this.post.slug
-        }
-      ]
-    };
-  },
-  computed: {
-    screen() {
-      console.log(window.screen.height);
-      return window.screen.height - 150;
-    }
+    const pages = await $content("docs")
+      .only(["title", "description", "img", "slug", "author"])
+      .fetch();
+    return { post, pages };
   }
 };
 </script>
