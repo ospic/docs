3f8b7f0cda44c2645a3a28572c0c49f8ceb9ec2e
=elirehema <=rocksandystone@gmail.com>
2021-02-20 15:05:56 +0300

    Re-Arrangement of vue mixins

--- mixins/mixins.js
+++ mixins/mixins.js
@@ -1,5 +1,17 @@
 import Vue from 'vue';
+
+import VImg from "~/components/VImg.vue";
+import VTags from "~/components/VTags.vue";
+import VNav from "~/components/VNav";
+import VPages from "@/components/VPages"
+
 Vue.mixin({
+  components:{
+    "VImg":VImg,
+    "VTags":VTags,
+    "VNav":VNav,
+    "v-pages": VPages
+  },
   data: function () {
     return {     }
   },
