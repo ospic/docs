46e9d6d075cbb7a599ebbc1ebf4fec8a09f7eaf3
=elirehema <=rocksandystone@gmail.com>
2021-02-21 13:55:30 +0300

    Filter locale tags

--- layouts/default.vue
+++ layouts/default.vue
@@ -75,17 +75,12 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: "mdi-apps",
-          title: "Welcome",
-          to: "/"
-        },
-        {
-          icon: "mdi-chart-bubble",
-          title: "Inspire",
-          to: "/inspire"
-        }
+
+      locales: [
+        { locale: "English", lang: "en" },
+        { locale: "Swahili", lang: "sw" },
+        { locale: "French", lang: "fr" },
+        { locale: "Arabic", lang: "ar" }
       ],
       miniVariant: false,
       right: true,
