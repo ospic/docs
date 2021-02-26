c9f2eeb583badfc955f668666942cea608021723
=elirehema <=rocksandystone@gmail>
2021-02-26 14:07:36 +0300

    Nuxt links hover

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -7,17 +7,17 @@
         :to="localePath({ name: 'docs-slug', params: { slug: prev.slug } })"
         class="text-primary font-weight-bold hover:underline"
       >
-       <v-icon color="primary" class="mb-1">mdi-chevron-left</v-icon> {{ prev.title }} 
+       <v-icon color="primary" class="mb-1" x-small>mdi-arrow-left</v-icon> {{ prev.title }} 
       </NuxtLink>
     </v-col>
     <v-col md="4"></v-col>
     <v-col md="4" class="d-flex flex-row-reverse ">
       <NuxtLink
-        v-if="next" prepend-icon="mdi-chevron--right"
+        v-if="next" prepend-icon="mdi-chevron-right"
         :to="localePath({ name: 'docs-slug', params: { slug: next.slug } })"
         class=" mt-auto font-weight-bold hover:underline"
       >
-        {{ next.title }} <v-icon color="primary" >mdi-chevron-right</v-icon>
+        {{ next.title }} <v-icon color="primary" class="mb-1" x-small >mdi-arrow-right</v-icon>
       </NuxtLink>
     </v-col>
   </div>
