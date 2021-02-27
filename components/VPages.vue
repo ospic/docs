abe11a63a829a80f2f2d05ecbf7dcb0008dac5cf
Elirehema <rocksandystone@gmail.com>
2021-02-27 18:55:57 +0300

    Add getting started title

--- components/VPages.vue
+++ components/VPages.vue
@@ -1,9 +1,11 @@
 <template>
    <nav>
+      <h5 class="gray--text lighten-2 text-uppercase">Getting started</h5>
+       <v-divider></v-divider>
       <ul>
+      
           <li v-for="page of pages" :key="page.slug" style="list-style-type: square;" class="pa-2" >
                <NuxtLink :to="localePath({ name: 'docs-slug', params: { slug: page.slug } })">{{page.title}}</NuxtLink>
-
                <hr class="dotted">
             </li>
           </ul>
