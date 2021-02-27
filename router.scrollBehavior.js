0ed71b8e62783173dad9699b3fda5fda60a127a6
Elirehema <rocksandystone@gmail.com>
2021-02-28 01:00:35 +0300

    Ovveride scroll behaviour

--- router.scrollBehavior.js
+++ router.scrollBehavior.js
@@ -1,8 +1,24 @@
-export default function (to, from, savedPosition) {
-    if (to.hash) {
-        return {
-          selector: to.hash
-          // , offset: { x: 0, y: 10 }
-        }
-      }
+export default function(to, from, savedPosition) {
+  const defaultPosition = false;
+  const scrollTopPosition = { x: 0, y: 0 };
+
+  let position = defaultPosition;
+  console.log({ to, from });
+  if (savedPosition) {
+    position = savedPosition;
+  } else if (to.matched.length < 2) {
+    position = scrollTopPosition;
+  } else if (to.matched.some(child => child.components.default.options.scrollToTop)) {
+    position = scrollTopPosition;
   }
+
+  return new Promise(resolve => {
+    window.$nuxt.$once('triggerScroll', () => {
+      if (to.hash && document.querySelector(to.hash)) {
+        position = { selector: to.hash };
+      }
+
+      resolve(position);
+    });
+  });
+}
