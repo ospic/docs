949a9428934f7b8a84ed26b300fa7e0d060022b3
Elirehema <rocksandystone@gmail.com>
2021-06-22 21:46:15 +0300

    Sponsor url

--- components/blog-page.vue
+++ components/blog-page.vue
@@ -22,7 +22,7 @@
       </v-avatar>
 
       <v-spacer></v-spacer>
-      <v-btn medium rounded outlined dark color="white" class="yellow accent-2 primary--text" href="https://github.com/sponsors/ospic?o=esb" target="_blank">
+      <v-btn medium rounded outlined dark color="white" class="yellow accent-2 primary--text" href="https://opencollective.com/ospic" target="_blank">
        <v-icon >mdi-heart-outline</v-icon>&nbsp;
        Sponsor</v-btn>&nbsp;&nbsp;
       <!--<iframe src="https://github.com/sponsors/ospic/button" title="Sponsor ospic" height="35" width="116" style="border: 0;"></iframe>-->
